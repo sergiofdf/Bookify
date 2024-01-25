dc5fd2ad7daad257d90037f760bd902ac343d8bd
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2024-01-24 23:37:51 -0300

    add users domain

---
+++ Bookify.Domain/Users/User.cs
@@ -0,0 +1,24 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Users;
+
+public sealed class User : Entity
+{
+	private User(Guid id, FirstName firstname, LastName lastName, Email email) : base(id)
+	{
+		FirstName = firstname;
+		LastName = lastName;
+		Email = email;
+	}
+
+	public FirstName FirstName { get; private set; }
+	public LastName LastName { get; private set; }
+	public Email Email { get; private set; }
+
+	public static User Create(FirstName firstname, LastName lastName, Email email)
+	{
+		var user = new User(Guid.NewGuid(), firstname, lastName, email);
+
+		return user;
+	}
+}
